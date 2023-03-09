1360922c3092a3874d2040e056dd692c1a9f0bb7
EdemKarashaev <just_2_feel@mail.ru>
2023-03-10 02:04:58 +0300

    json

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -1,22 +1,24 @@
 
-
 import java.io.*;
 
 public class Basket implements Serializable {
-     String[] products;
-     int[] prices;
-     int[] cartBasket;
-
-    Basket() {
-    }
+    String[] products;
+    int[] prices;
+    int[] cartBasket;
 
     public Basket(String[] products, int[] prices) {
         this.products = products;
         this.prices = prices;
-        this.cartBasket = new int[prices.length];
+        this.cartBasket = new int[products.length];
         for (int i = 0; i < products.length; i++) {
             cartBasket[i] = 0;
         }
+    }
+
+    public Basket(String[] products, int[] prices, int[] cartBasket) {
+        this.products = products;
+        this.prices = prices;
+        this.cartBasket = cartBasket;
     }
 
     public void addToCart(int productNum, int amount) {
@@ -35,28 +37,34 @@ public class Basket implements Serializable {
     public void saveTxt(File file) throws IOException {
         try (FileWriter saveFile = new FileWriter(file)) {
             for (int i = 0; i < products.length; i++) {
-                saveFile.write(products[i]  +" "+ cartBasket[i]+" ");
+                saveFile.write(products[i] + " " + prices[i] + " " + cartBasket[i] + " ");
             }
         }
     }
 
     public static Basket loadFromTxtFile(File file) throws IOException {
         try (FileReader loadFile = new FileReader(file)) {
+
             StringBuilder s = new StringBuilder();
             int c = 0;
             while ((c = loadFile.read()) != -1) {
                 s.append(Character.toChars(c));
             }
             String[] k = s.toString().split(" ");
-            Basket basket = new Basket();
-            products = products;
-            prices = prices;
-            cartBasket = new int[k.length/2];
+            String[] prod = new String[k.length/3];
+            int[] pr = new int[k.length/3];
+            int cart[] = new int[k.length/3];
 
-            for (int i = 0; i < k.length/2; i++) {
-                cartBasket[i] = Integer.parseInt(k[i*2+1]);
+            for (int i = 0; i < k.length / 3; i++) {
+                prod[i] = (k[i * 3]);
+                pr[i] = Integer.parseInt(k[(i * 3) + 1]);
+                cart[i] = Integer.parseInt(k[(i * 3) + 2]);
             }
+
+            Basket basket = new Basket(prod, pr,cart);
             return basket;
         }
     }
 }
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,43 +2,110 @@
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, TransformerException, SAXException {
 
         String[] products = {"яблоки", "хлеб", "молоко", "печенье"};
         int[] prices = {58, 45, 98, 76};
         int[] cartBasket;
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+        Element config = document.createElement("Config");
+        document.appendChild(config);
+
+        Element load = document.createElement("load");
+        config.appendChild(load);
+
+        Element enable = document.createElement("enable");
+        load.appendChild(enable);
+        enable.setTextContent("false");
+        Element fileName = document.createElement("fileName");
+        load.appendChild(fileName);
+        fileName.setTextContent("Basket.json");
+        Element format = document.createElement("format");
+        load.appendChild(format);
+        format.setTextContent("json");
+
+        Element save = document.createElement("save");
+        config.appendChild(save);
+
+        Element enable1 = document.createElement("enable");
+        save.appendChild(enable1);
+        enable1.setTextContent("true");
+        Element fileName1 = document.createElement("fileName");
+        save.appendChild(fileName1);
+        fileName1.setTextContent("Basket.json");
+        Element format1 = document.createElement("format");
+        save.appendChild(format1);
+        format1.setTextContent("json");
+
+        Element log = document.createElement("log");
+        config.appendChild(log);
+        Element enable2 = document.createElement("enable");
+        save.appendChild(enable2);
+        enable2.setTextContent("true");
+        Element fileName2 = document.createElement("fileName");
+        save.appendChild(fileName2);
+        fileName1.setTextContent("Client.csv");
+
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File("Shop.xml"));
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.transform(domSource, streamResult);
+
+        Document doc = builder.parse(new File("Shop.xml"));
+        Node root = doc.getDocumentElement();
+        NodeList con = root.getChildNodes();
+        NodeList con1 = con.item(0).getChildNodes();
+
+        if (con1.item(0).getTextContent().equals("true")) {
+
+
+        }
+
+// Basket basket1 = Basket.loadFromTxtFile(new File("BasketAmounts.txt"));
+
+
         JSONParser parser = new JSONParser();
         try {
             Object obj = parser.parse(new FileReader("Basket.json"));
             JSONObject basketJSON = (JSONObject) obj;
             int[] v = new int[products.length];
-            for (int i = 0; i < products.length; i++) {
-                v = (int[]) basketJSON.get("basket.cartBasket[]");
-            }
-            Basket basket = new Basket(products, prices) {
-
+       /*     for (Object key: basketJSON.keySet()) {
+                v = (int[]) key;
+            }*/
+            Basket basket = new Basket(products, prices, v) {
             };
-
-
-            // Basket basket1 = Basket.loadFromTxtFile(new File("BasketAmounts.txt"));
-
         } catch (ParseException e) {
             e.printStackTrace();
         } finally {
             Basket basket = new Basket(products, prices);
-
             System.out.println("список возможных товаров за килограмм, литр, штуку:");
             for (int i = 0; i < products.length; i++) {
                 System.out.println(i + 1 + "  " + products[i] + " по цене  " + prices[i] + "  за килограмм, литр, штуку");
             }
-
             ClientLog cl = new ClientLog();
             basket.addToCart(3, 12);
             basket.addToCart(1, 8);
@@ -50,10 +117,9 @@ public class Main {
             //basket.saveTxt(new File("BasketAmounts.txt"));
             cl.exportAsCSV(new File("log.csv"));
 
-
             JSONObject basketJSON = new JSONObject();
             for (int i = 0; i < products.length; i++) {
-                basketJSON.put(Basket.products[i], basket.cartBasket[i]);
+                basketJSON.put(products[i], basket.cartBasket[i]);
             }
             try (FileWriter writer = new FileWriter("Basket.json")) {
                 writer.write(basketJSON.toString());
